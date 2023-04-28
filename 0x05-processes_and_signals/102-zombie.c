2faafa922071966151dc8dc335f444c527a1bac0
Tobi Tijani <tobi_tijani@yahoo.com>
2023-04-28 12:28:26 -0700

    Task 12

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - an infinite while loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates zombie processes
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int counter = 0;
+
+	while (counter < 5)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+			return (1);
+		if (child_pid == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+		counter++;
+	}
+	infinite_while();
+	return (0);
+}
