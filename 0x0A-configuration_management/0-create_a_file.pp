a1e67cd1173bd4c61195d2050bf814dadf88a1bd
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-23 11:18:23 -0700

    Task 0 fix: added commas to the end of the statement

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file in /tmp
 file { '/tmp/school':
-  ensure  => present
-  mode    => '0744'
-  owner   => 'www-data'
-  group   => 'www-data'
-  content => 'I love Puppet'
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
