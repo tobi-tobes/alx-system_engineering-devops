e6eb75b92aceedda4a36fe78f6258cddc0900678
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-25 11:14:34 -0700

    Advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# sets up your SSH configuration file to connect to server without a password
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'BatchMode yes',
+  match  => '^BatchMode',
+}
