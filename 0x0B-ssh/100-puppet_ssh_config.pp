d258f0b05594a0974ceead7870ddf4c38f930625
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-25 12:09:21 -0700

    Task 4 changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,19 @@
 # sets up your SSH configuration file to connect to server without a password
-file { '~/.ssh/config':
+file { '~/.ssh/ssh_config':
   ensure => present,
 }
 
 file_line { 'Host':
-  path => '~/.ssh/config',
-  line => 'Host *',
+  path => '~/.ssh/ssh_config',
+  line => 'Host 54.160.124.170',
 }
 
 file_line { 'Declare identity file':
-  path => '~/.ssh/config',
-  line => '    IdentityFile ~/.ssh/school',
+  path => '~/.ssh/ssh_config',
+  line => '     IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off password authentication':
-  path => '~/.ssh/config',
-  line => '    BatchMode yes',
+  path => '~/.ssh/ssh_config',
+  line => '     BatchMode yes',
 }
