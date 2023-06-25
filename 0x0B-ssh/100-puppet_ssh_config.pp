c512f998ac5f36829fb98d7a27f188cc3d6a22ca
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-25 11:44:44 -0700

    Changed file_line configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,12 +5,12 @@ file { '/etc/ssh/ssh_config':
 
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
+  line => '#   IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off password authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'BatchMode yes',
-  match  => '^BatchMode',
+  line   => '#   BatchMode yes',
+  match  => '^#\ \ \ BatchMode',
 }
