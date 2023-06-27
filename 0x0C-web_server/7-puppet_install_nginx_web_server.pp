eb9391efd3f5e6e3a3b958c4df76f3ba133eccf4
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-27 16:15:29 -0700

    Task 5 fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,18 @@
 # Installs and configures Nginx Web Server
 exec { 'update':
-  command => 'apt-get -y update',
-  path    => '/usr/bin/',
+  command => 'apt-get -y update',
+  path    => '/usr/bin/',
 }
-
 package { 'nginx':
-  ensure   => installed,
-  provider => 'apt',
+  ensure   => installed,
+  provider => 'apt',
 }
-
 file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
+  ensure  => present,
+  content => 'Hello World!\n',
 }
-
 file_line { 'create code 301 redirect':
-  path  => '/etc/nginx/sites-enabled/default',
-  line  => '\ \ \ \ location \/redirect_me { return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4\$request_uri; }',
-  after => '^\s+server_name\s_;',
+  path  => '/etc/nginx/sites-enabled/default',
+  line  => '        rewrite ^/redirect_me$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  after => '^\s+server_name _;',
 }
