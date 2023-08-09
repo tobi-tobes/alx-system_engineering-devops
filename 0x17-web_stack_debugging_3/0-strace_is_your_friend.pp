4d34ec3b1d4d6fee2d858b4f5f7630f8ede79d42
Tobi Tijani <tobi_tijani@yahoo.com>
2023-08-09 03:56:30 -0700

    Task 0 fixed typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes typo in /var/www/html/wp-setting.php file
+# Fixes typo in /var/www/html/wp-settings.php file
 
 exec { 'fix typo':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-setting.php",
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
