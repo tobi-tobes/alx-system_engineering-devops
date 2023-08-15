c549c535c5fea21c1d3ee05b2bc645d20c2209d5
Tobi Tijani <tobi_tijani@yahoo.com>
2023-08-15 13:22:44 -0700

    Task 1 fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,16 +1,18 @@
 # Fixes limit issue for user holberton
 
-exec { 'raise limit':
+exec { 'raise hard limit':
   command => "sed -i 's|holberton\shard\snofile\s5|holberton\shard\snofile\s4096|g' /etc/security/limits.conf",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec { 'raise limit':
+exec { 'raise soft limit':
   command => "sed -i 's|holberton\ssoft\snofile\s4|holberton\ssoft\snofile\s4096|g' /etc/security/limits.conf",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['raise hard limit'],
 }
 
 exec { 'restart system':
   command => 'sysctl -p',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['raise soft limit'],
 }
